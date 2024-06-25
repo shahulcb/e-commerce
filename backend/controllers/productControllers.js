6e03b9c51d11a573bba4ecc6e65a211c8ff82562
shahul cb <theshahulofcl@gmail.com>
2024-06-26 00:56:07 +0530

    product category added

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -7,9 +7,9 @@ export const getProducts = catchAsyncErrors(async (req, res) => {
   const resPerPage = 4;
   const apiFilters = new APIFilters(Product, req.query).search().filters();
   let products = await apiFilters.query;
+  let filteredProductsCount = products.length;
   apiFilters.pagination(resPerPage);
   products = await apiFilters.query.clone();
-  let filteredProductsCount = products.length;
   res.status(200).json({
     resPerPage,
     filteredProductsCount,
